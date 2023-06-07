ebfa49d20d109536eecf931301b78e3f5377c654
Dalen Ward <dalen.ward@m6securities.com>
2023-06-06 21:39:16 -0600

    Initial Commit

---
+++ Simple Journal/Simple_JournalApp.swift
@@ -0,0 +1,20 @@
+//
+//  Simple_JournalApp.swift
+//  Simple Journal
+//
+//  Created by Dalen Ward on 6/6/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Simple_JournalApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
